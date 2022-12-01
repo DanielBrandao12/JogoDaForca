7aae11dfeb031fc75dd14c4515cc1b393c147a15
DanielBrandao12 <drb12da@gmail.com>
2022-12-01 15:52:22 -0300

    função para diferenciar letras com acentos

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const table = document.querySelector('table')
 const forca = document.querySelector('.img')
 const listaAlfabeto = document.querySelector('.lista-letras')
 
-
+//Lista de imagens de sequencia da forca
 let backgrounds = [
     'url("img/1.png")',
     'url("img/2.png")',
@@ -15,6 +15,8 @@ let backgrounds = [
     'url("img/6.png")',
     'url("img/7.png")'
 ]
+
+//Lista de palavras provisorio
 let palavras = [
     'MAÇA',
     'PERA',
@@ -23,29 +25,33 @@ let palavras = [
 ]
 let alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","U","V","W","X","Y","Z"]
 
+//cria o tabuleiro de letras
 alfabeto.forEach(e=>{
     let divAlfabeto = document.createElement('div')
  
     divAlfabeto.innerHTML = `<button id="btnLetra">${e}</button>`
     listaAlfabeto.appendChild(divAlfabeto)
 })
+
 const btnLetra = document.querySelectorAll('#btnLetra')
 
 
-let index=1
-
-
+/*
 btn.addEventListener('click', e=>{
-
+    
     forca.style.background = backgrounds[i]
     forca.style.backgroundSize = 'cover'
     i++
 })
+*/
 
 let arr = []
+
 btnSortear.addEventListener('click', e =>{
+
     let qtdePalavra =[]
     let div = document.createElement('div')
+
     const palavra = Math.floor(Math.random() * palavras.length)
     
     for(let i= 0; i< palavras[palavra].length;i++){
@@ -53,32 +59,52 @@ btnSortear.addEventListener('click', e =>{
         //console.log(arr)
         qtdePalavra.push(`<div class="linha" ><span>${arr[i]}</span></div>`)
         div.innerHTML = qtdePalavra.join('')
-       // console.log(arr)
+        // console.log(arr)
     }
+
     divLinha.appendChild(div)
+  
+  
     const quadroLetra = document.querySelectorAll('.linha span')
-    
-    //Encontrar algum jeito de fazer o teste sem usar o for
-    for(let i =0; i< quadroLetra.length;i++){
-        
-        btnLetra.forEach(e =>{
-            e.addEventListener('click', () =>{
-                if(e.innerText == arr[i]){
-                    quadroLetra[i].style.visibility = 'visible'
-                    console.log( e.innerText + 'oi')
-                }else{
-                    forca.style.background = backgrounds[index]
-                    forca.style.backgroundSize = 'cover'
-                    
-                }
-                
-                index++
+
+    let i =0
+    let j =1
+   
+  
+
+       btnLetra.forEach(e =>{
+           e.addEventListener('click', () =>{
+            console.log(e.innerText)
+            console.log(arr[arr.indexOf(e.innerText)])
+               if(e.innerText ===replaceSpecialChars( arr[arr.indexOf(e.innerText)])){
+                quadroLetra[i].style.visibility = 'visible'
+                   console.log( e.innerText + ' '+i)
+                   i++
+                   e.disabled = true
+               }else{
+                console.log(i)
+                   forca.style.background = backgrounds[j]
+                   forca.style.backgroundSize = 'cover'
+                 j++
+               }
+            console.log(arr[3])
+               
             })
-        })
-        console.log(quadroLetra[i])
-        
-    }
-    return arr
+       })
 })
 
 
+    
+
+function replaceSpecialChars(str)
+{
+    str = str.replace(/[ÀÁÂÃÄÅ]/,"A");
+    str = str.replace(/[àáâãäå]/,"a");
+    str = str.replace(/[ÈÉÊË]/,"E");
+    str = str.replace(/[Ç]/,"C");
+    str = str.replace(/[ç]/,"c");
+
+    // o resto
+
+    return str.replace(/[^a-z0-9]/gi,''); 
+}
